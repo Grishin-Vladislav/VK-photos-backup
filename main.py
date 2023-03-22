314567fe79aa8c6a9ddef25604aca9ba27701b1a
GVladislav <grishinvladislav97@gmail.com>
2023-03-22 22:52:34 +0300

    Fix get_meta() to take the biggest sizes

--- main.py
+++ main.py
@@ -37,22 +37,39 @@ def is_valid_user_id(user_id: str, user_interface: VkUsers) -> bool:
 
 
 def get_meta_from_photo_album(album: dict, amount: dict) -> dict:
+    # todo: split this func
     """
     Forms metadata needed to upload files to cloud.
-    :param album: dictionary from vkapi request.
+    :param album: dictionary from vkapi/photos.get request.
     :param amount: desired amount of photos to upload.
-    :return:
+    :return: dictionary with metadata which also includes filename.
     """
     meta = {}
     for indx, item in enumerate(album['response']['items'][:amount], 1):
         date = datetime.fromtimestamp(item['date']).strftime('%d.%m.%Y_%H.%M')
         likes = int(item['likes']['count'])
+
+        has_max_resolution = False
+        max_res_index = 0
+        for i, size in enumerate(item['sizes']):
+            if size['type'] == 'w':
+                has_max_resolution = True
+                max_res_index = i
+                break
+
+        if has_max_resolution:
+            res_type = 'w'
+            url = item['sizes'][max_res_index]['url']
+        else:
+            res_type = item['sizes'][-1]['type'],
+            url = item['sizes'][-1]['url']
+
         meta[f'photo{indx}'] = {
             'date': date,
             'likes': likes,
             'photo': {
-                'type': item['sizes'][-1]['type'],
-                'url': item['sizes'][-1]['url']
+                'type': res_type,
+                'url': url
             }
         }
 
@@ -83,3 +100,4 @@ if __name__ == '__main__':
         amount = int(input(f'В альбоме {profile_photos["response"]["count"]}'
                            f' фото, сколько последних фото сохранить?\n'))
         meta = get_meta_from_photo_album(profile_photos, amount)
+        pprint(meta)
